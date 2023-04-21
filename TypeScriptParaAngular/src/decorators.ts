f9c4a95130e2993c34740b65af59bf1924bee9a4
birajnior <ubiratanbernardo@gmail.com>
2023-04-21 17:19:30 -0300

    Finalizando aulas de tipos

---
+++ TypeScriptParaAngular/src/decorators.ts
@@ -0,0 +1,39 @@
+// decorators
+function apiVersion(version: string) {
+  return (target: any) => {
+    Object.assign(target.prototype, { __version: version, __name: "JB" });
+  };
+}
+
+// attribute decorator
+function minlength(length: number) {
+  return (target: any, key: string) => {
+    let _value = target[key];
+
+    const getter = () => _value;
+    const setter = (value: string) => {
+      if (value.length < length) {
+        throw new Error(`Tamanho menor do que ${length}`);
+      } else {
+        _value = value;
+      }
+    };
+
+    Object.defineProperty(target, key, {
+      get: getter,
+      set: setter,
+    });
+  };
+}
+
+class Api {
+  @minlength(3)
+  name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+}
+
+const api = new Api("Olá");
+console.log(api.name);
