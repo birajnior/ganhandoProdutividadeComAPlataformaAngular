6a4057001963fa6ab3a82966e3542aa2b504d0e3
birajnior <ubiratanbernardo@gmail.com>
2023-04-28 22:09:59 -0300

    Atualizando aula

---
+++ trabalhandoComComponentesAngular/src/app/components/menu.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+selector: 'menu-component',
+template: '<h2>JB</h2>',
+styles: ['h2{ color: coral}'],
+})
+
+export class MenuComponent {
+
+}
